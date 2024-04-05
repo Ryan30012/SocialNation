2867f6b20815da7aab6f9a9d1ca3aeef7b740e1f
Ryan30012 <ryan.sef1@gmail.com>
2024-04-05 05:03:42 -0400

    new commits to start

---
+++ components/convos/ConversationList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ConversationList = () => {
+
+  return (
+    <div className=''>
+      <div className=''>
+
+      </div>
+    </div>
+  );
+};
+
+export default ConversationList
